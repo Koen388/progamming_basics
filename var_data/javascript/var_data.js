516fc42b2540b8cd03adea93b8c85b5841a2aebb
Koen388 <Koen388@gmail.com>
2018-09-19 12:39:30 +0200

    2 Opdrachten oef

---
+++ var_data/javascript/var_data.js
@@ -0,0 +1,14 @@
+//Opdracht 1
+let a = 10;
+if(a%2==0) {
+    console.log("The number 10 is even");
+} else {
+    console.log("The number 10 is Odd");
+}
+
+//Opdracht 2
+let pmgNotCool = "Programming isnot so cool"
+// console.log(pmgNotCool);
+
+pmgCool = pmgNotCool.replace("not", "");
+console.log(pmgCool);
