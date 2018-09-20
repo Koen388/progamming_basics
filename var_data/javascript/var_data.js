64c0f897f0f6e51b4462a91b5f061720393bae7a
Koen388 <Koen388@gmail.com>
2018-09-20 12:34:23 +0200

    made this

--- var_data/javascript/var_data.js
+++ var_data/javascript/var_data.js
@@ -12,3 +12,12 @@ let pmgNotCool = "Programming isnot so cool"
 
 pmgCool = pmgNotCool.replace("not", "");
 console.log(pmgCool);
+
+//Opdracht 3
+const Number = 1600;
+const Text = "Ik woon in Naboo";
+if(Number == Text) {
+    console.log("De twee variabelen zijn gelijk")
+} else {
+    console.log("De twee variabelen zijn niet gelijk")
+}
