eca0a2aef003a83b913bb2e5f63133bc58a6edb6
Koen388 <Koen388@gmail.com>
2018-09-20 13:00:19 +0200

    Nieuwe opdrachten erbij gedaan

---
+++ conditionals/conditionals/conditionals.js
@@ -0,0 +1,10 @@
+let cijfer = 7;
+if(cijfer < 6) {
+    console.log("Je hebt een onvoldoende.")
+} else if(cijfer < 7) {
+    console.log("Je hebt een voldoende!")
+} else if(cijfer < 9) {
+    console.log("Je hebt een goed!")
+} else {
+    console.log("Je hebt een uitmuntend!")
+}
